3386cc74bf681aa7924f3ef471d45cf66ee96b88
chanderquichiz <chanderquichiz@outlook.com>
2026-08-31 23:30:03 -0700

    resolviendo problemas de type

--- field/src/field/dto/update-field.dto.ts
+++ field/src/field/dto/update-field.dto.ts
@@ -3,7 +3,7 @@ import { CreateFieldDto } from './create-field.dto';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class UpdateFieldDto extends PartialType(CreateFieldDto) {
-    @IsNotEmpty()
-    @IsNumber()
-    id: number;
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
 }
